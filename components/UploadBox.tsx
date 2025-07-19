22754f3c3608201dce3e6299f70d7606cb51fb41
daman599 <damanpreetk117@gmail.com>
2025-07-19 17:55:27 +0530

    drag and drop feature added

--- components/UploadBox.tsx
+++ components/UploadBox.tsx
@@ -4,31 +4,41 @@ import { UploadCloud } from 'lucide-react';
 import { useRef, useState } from "react";
 import axios from "axios";
 import Loader from "./Loader";
+import { useDropzone } from 'react-dropzone';
 
 export default function UploadBox() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [loading ,setLoading] = useState<Boolean>(false);
-   
-  async function handleOnChange(e: React.ChangeEvent<HTMLInputElement>){
+  const [loading, setLoading] = useState<Boolean>(false);
+  const [selectFile ,setSelectFile] = useState<Boolean>(false);
+
+  async function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
     const file = e.target.files?.[0];
 
-    if(file){
+    if (file) {
       setLoading(true);
-       axios.post("http://localhost:3000/api/resume/analyse",{
-        "resume":file
-      });
+      setSelectFile(true);
     }
   }
+  
+  const { getRootProps, getInputProps, acceptedFiles } = useDropzone({
+    accept: { 'application/pdf': ['.pdf'] },
+    maxFiles: 1,
+    onDrop: (acceptedFiles) => {
+    
+      setLoading(true);
+    },
+     noClick: true,
+  });
 
   if (loading) {
-   return (
+    return (
       <Loader />
     );
-}
+  }
 
   return (
     <div className="min-h-screen flex items-center justify-center px-4 py-12">
-      <div
+      <div {...getRootProps()}
         className="w-full max-w-md sm:max-w-xl p-4 sm:p-6 md:p-8
              rounded-3xl border border-white/20 border-dashed 
              bg-white/5 backdrop-blur-xl shadow-2xl 
@@ -36,22 +46,24 @@ export default function UploadBox() {
              space-y-4 text-center
              hover:border-blue-400 transition-all duration-300">
 
-        <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-white">
-          Upload Your Resume
-        </h1>
-
-        <p className="text-xs sm:text-sm md:text-base text-gray-300 max-w-sm">
-          Let our AI match your resume with the perfect job opportunities.
-        </p>
+        <input {...getInputProps()} />
 
         <UploadCloud
           className="text-blue-400 animate-pulse"
           size={40}
         />
 
+        <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-white">
+          Drag and drop your resume here
+        </h1>
+
+        <p className="text-xs sm:text-sm md:text-base text-gray-300 max-w-sm">
+          Or
+        </p>
+
         <button
-          onClick={()=>{
-             inputRef.current?.click();
+          onClick={() => {
+            inputRef.current?.click();
           }}
           className="px-5 py-2.5 sm:px-6 sm:py-3 rounded-md text-sm sm:text-base
                bg-white/10 border border-white/20 text-white font-medium
