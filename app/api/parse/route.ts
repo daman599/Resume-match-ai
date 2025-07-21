84bc5e6af932abcb268ec58aefd0c04466762fac
daman599 <damanpreetk117@gmail.com>
2025-07-21 23:36:01 +0530

    organised it

--- app/api/parse/route.ts
+++ app/api/parse/route.ts
@@ -1,6 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import PDFParser from "pdf2json";
 
+type ParsedPDFtype = {
+       Pages: {
+              Texts: {
+                     R: any[];
+              }[];
+       }[];
+};
 export async function POST(request: NextRequest) {
        const formData = await request.formData();
        const file = formData.get("resume") as File;
@@ -8,14 +15,6 @@ export async function POST(request: NextRequest) {
        const buffer = Buffer.from(await file.arrayBuffer());
 
        const pdfParser = new PDFParser();
-
-       type ParsedPDFtype = {
-              Pages: {
-                     Texts: {
-                            R: any[];
-                     }[];
-              }[];
-       };
 
        const parsedData: ParsedPDFtype = await new Promise((resolve, reject) => {
 
@@ -29,11 +28,11 @@ export async function POST(request: NextRequest) {
               });
        })
 
-       const parsedText = parsedData.Pages.flatMap((p)=>{
-          return p.Texts.map((t)=>{
-              return decodeURIComponent(t.R[0].T).replace(/\s+/g, " ")
-           })
+       const parsedText = parsedData.Pages.flatMap((p) => {
+              return p.Texts.map((t) => {
+                     return decodeURIComponent(t.R[0].T).replace(/\s+/g, " ")
+              })
        }).join(" ");
 
-       return NextResponse.json({ "parsedText" : parsedText});
+       return NextResponse.json({ "parsedText": parsedText });
 }
