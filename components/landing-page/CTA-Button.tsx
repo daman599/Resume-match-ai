49d6df47197d450058bf666fe0aafdbb9d32ee99
daman599 <damanpreetk117@gmail.com>
2025-07-21 21:37:04 +0530

    made navigation smooth

--- components/landing-page/CTA-Button.tsx
+++ components/landing-page/CTA-Button.tsx
@@ -1,16 +1,20 @@
-import Link from 'next/link';
+'use client'
+
 import { UploadCloud } from 'lucide-react';
+import { useRouter } from "next/navigation";
 
 export default function CTAButton() {
+  const router = useRouter();
+
   return (
     <div className="mt-8">
-      <Link
-        href="/resume-upload"
-        className="inline-flex items-center gap-2 px-6 py-3 border border-white text-white font-semibold rounded-lg hover:bg-white hover:text-black transition-all duration-200 text-sm sm:text-base"
+      <button
+        onClick={()=>{router.push("/resume-upload")}}
+        className="inline-flex items-center gap-2 px-6 py-3 border border-white text-white font-semibold rounded-lg hover:bg-white hover:text-black transition-all duration-200 text-sm sm:text-base cursor-pointer"
       >
         <UploadCloud className="w-5 h-5" />
         Upload Your Resume
-      </Link>
+      </button>
     </div>
   );
 }
