a995c196fd07e872833144ac7cdaf8dafbf1bba6
daman599 <damanpreetk117@gmail.com>
2025-07-22 13:16:58 +0530

    results route added

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,5 +1,32 @@
-export default function Results(){
+'use client'
+
+import useStore from "@/store";
+import axios from "axios";
+import Loader from "@/components/Loader";
+import { useState, useEffect } from "react";
+
+export default function Results() {
+
+    const [loading, setLoading] = useState<boolean>(true);
+    const parsedText = useStore((state) => (state.parsedText));
+
+    async function APIcall() {
+        const response = await axios.post("http://localhost:3000/api/ai-processing",
+            { resumeText: parsedText }
+        )
+        console.log(response.data);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        APIcall();  
+    }, [])
+
+    if (loading) {
+        return <Loader />
+    }
+
     return (
-        <>HI THERE</>
+        <div className="text-red-800">Results ............</div>
     );
 }
