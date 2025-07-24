bfc49764d0b033d9efc94942bc630c193ea5c069
daman599 <damanpreetk117@gmail.com>
2025-07-24 13:18:54 +0530

    made route clean and readable

--- app/api/ai-processing/route.ts
+++ app/api/ai-processing/route.ts
@@ -1,8 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import OpenAI from 'openai';
 import mongoose from "mongoose";
-import axios from "axios";
-import JobModel from "@/db";
+import { Jobs } from "../jobs/route"
 
 await mongoose.connect(process.env.MONGODB_URI!);
 
@@ -11,33 +10,13 @@ const groq = new OpenAI({
     baseURL: "https://api.groq.com/openai/v1",
 });
 
-const app_id = process.env.APP_ID;
-const app_key = process.env.APP_KEY;
-
 export async function POST(req: NextRequest) {
-
-    const URL = `https://api.adzuna.com/v1/api/jobs/in/search/1?app_id=${app_id}&app_key=${app_key}&results_per_page=20&sort_by=date`
-    const response = await axios.get(URL);
-    const results = response.data.results;
-
-    results.map(async (job:any) => {
-        const existing = await JobModel.findOne({jobId : job.id});
-        if(!existing){
-        await JobModel.create({
-            jobId:job.id,
-            title:job.title,
-            company:job.company.display_name,
-            location:job.location.area.join(", "),
-            jobCategory: job.category.label,
-            redirect_url:job.redirect_url,
-            description:job.description,
-            datePosted: new Date(job.created),
-        })
-       }
-    })
+    
     const body = await req.json();
     const resumeText = body.resumeText;
+    
+    const res = await Jobs();
+    console.log("jobs",await res.json());
 
     return NextResponse.json({ "done": "processing done" })
-
 }
