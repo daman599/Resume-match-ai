a94e604ac4aca9ea65628a848018dc2f7ba8823a
daman599 <damanpreetk117@gmail.com>
2025-07-28 15:18:22 +0530

    some changes

--- lib/state-store/store.ts
+++ lib/state-store/store.ts
@@ -5,12 +5,11 @@ type StateType = {
     updateParsedText : (text : string) => void 
 }
 
-const useStore = create<StateType>((set) =>  ({
-
-    parsedText : "",
-    updateParsedText : (text) => set({ parsedText : text })
-    
-    })
-)
+const useStore = create<StateType>((set) => (
+     {
+        parsedText : "", 
+        updateParsedText : (text) => set({ parsedText : text })
+     }
+))
 
 export default useStore;
