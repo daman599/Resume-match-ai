b46c9b3b6fc1023e2aaed7e193736266d239fde1
daman599 <damanpreetk117@gmail.com>
2025-07-28 12:11:40 +0530

    put error component in the center of page

--- components/helperComponents/Error.tsx
+++ components/helperComponents/Error.tsx
@@ -1,9 +1,11 @@
 const ErrorComponent = () =>{
   return (
+  <div className="flex items-center justify-center h-screen w-screen">
     <div className="flex flex-col items-center justify-center space-y-2 text-center text-white">
-      <p className="font-inter font-semibold text-xl">Something went wrong........</p>
+      <p className="font-inter font-semibold text-xl">Something went wrong.</p>
       <p className="font-inter text-sm ">Try again later.</p>
     </div>
+  </div>
   );
 }
 
