3de58441b3413c26f3fe5702f1b59fe4ee23a769
daman599 <damanpreetk117@gmail.com>
2025-07-29 21:11:44 +0530

    blur text added

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -5,8 +5,10 @@ import axios from "axios";
 import Loader from "@/components/helperComponents/Loader";
 import { useState, useEffect } from "react";
 import ErrorComponent from "@/components/helperComponents/Error";
-import { Building2 , MapPin , Dot } from "lucide-react";
-import { inter } from "@/lib/fonts";
+import { Building2, MapPin, Dot } from "lucide-react";
+import { inter, plusJakarta } from "@/lib/fonts";
+import { useRouter } from "next/navigation";
+import BlurText from "@/Reactbits/BlurText/BlurText";
 
 interface Jobtype {
     "_id": string,
@@ -26,7 +28,8 @@ export default function Jobs() {
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<boolean>(false);
     const parsedText = useStore((state) => (state.parsedText));
-    const [jobs, setJobs] = useState<Jobtype[] | null>(null);
+    const [jobs, setJobs] = useState<Jobtype[]>([]);
+    const router = useRouter();
 
     async function APIcall() {
         try {
@@ -61,42 +64,45 @@ export default function Jobs() {
         <>
             {error && <ErrorComponent />}
             {jobs && jobs.length === 0 && <p className="text-white">No jobs found matching your profile.</p>}
-            {jobs && jobs.length > 0 && (
-                <div className="text-white px-4 py-20">
-                    <p className="text-lg mb-4">Here are the latest jobs matching your profile...</p>
+            <div className=" px-4 py-20">
+                <BlurText
+                    text="Here are the latest jobs matching to your profile..."
+                    delay={150}
+                    animateBy="words"
+                    direction="top"
+                    className="text-4xl mb-4 mt-2 ml-28 ${plusJakarta.variable} font-semibold text-blue-600"
+                />
 
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-                        {jobs.map((job) => (
-                            <div key={job.jobId} className="border-2 border-amber-950 bg-gray-300 p-4 rounded-md shadow-md text-black">
-                                <p className="font-bold text-lg">{job.title}</p>
-                                <div className="flex gap-2"> 
-                                    <Building2/> 
-                                    <p className="text-xl font-semibold">{job.company}</p>
-                                </div>
-                                <div className="flex gap-2">
-                                    <MapPin/>
-                                    <p className="text-sm">{job.location}</p>
-                                </div>
-                                <div className="flex gap-2">
-                                    <Dot/>
-                                    <p className="text-sm italic">{job.jobCategory}</p>
-                                </div>
-                                <a href={job.redirect_url} className="text-blue-600 underline" target="_blank" rel="noopener noreferrer">
-                                    View Job
-                                </a>
-                                <p className="line-clamp-3 text-sm text-gray-800 mt-2">{job.description}</p>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                    {jobs.map((job) => (
+                        <div key={job.jobId} className="border-2 border-amber-950 bg-gray-300 p-4 rounded-md shadow-md text-black">
+                            <p className="font-bold text-lg">{job.title}</p>
+                            <div className="flex gap-2">
+                                <Building2 />
+                                <p className="text-xl font-semibold">{job.company}</p>
                             </div>
-                        ))}
-                    </div>
+                            <div className="flex gap-2">
+                                <MapPin />
+                                <p className="text-sm">{job.location}</p>
+                            </div>
+                            <div className="flex gap-2">
+                                <Dot />
+                                <p className="text-sm italic">{job.jobCategory}</p>
+                            </div>
+                            <a href={job.redirect_url} className="text-blue-600 underline" target="_blank" rel="noopener noreferrer">
+                                View Job
+                            </a>
+                            <p className="line-clamp-3 text-sm text-gray-800 mt-2">{job.description}</p>
+                        </div>
+                    ))}
                 </div>
-            )}
-
-             <div className="flex items-center justify-center h-screen w-screen">
                 <div className="flex flex-col items-center justify-center space-y-2 text-center text-gray-300 text-xl">
-                   <button className={` bg-black w-[300px] h-[50px] p-2 cursor-pointer border-black rounded-2xl ${inter.variable}`}>Get tips to optimize resume.</button>
+                    <button onClick={() => {
+                        router.push("/resume-optimize");
+                    }}
+                        className={` bg-black w-[300px] h-[50px] p-2 cursor-pointer border-black rounded-2xl ${inter.variable}`}>Get tips to optimize resume.</button>
                 </div>
             </div>
-
         </>
     );
 }
