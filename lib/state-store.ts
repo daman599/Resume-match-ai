b6714fb79b5bcd844be1c76c46a9df8a405a275f
daman599 <damanpreetk117@gmail.com>
2025-07-30 11:45:58 +0530

    new state added

--- lib/state-store.ts
+++ lib/state-store.ts
@@ -1,14 +1,31 @@
 import { create } from 'zustand'
 
+type JobType = {
+    _id: string,
+    jobId: string,
+    title: string,
+    location: string,
+    company: string,
+    jobCategory: string,
+    redirect_url: string,
+    description: string,
+    createdAt: string,
+    __v: number,
+}
+
 type StateType = {
     parsedText : string,
-    updateParsedText : (text : string) => void 
+    updateParsedText : (text : string) => void ,
+    jobs : JobType[],
+    updateJobs : (jobs :JobType[]) => void ,
 }
 
 const useStore = create<StateType>((set) => (
      {
         parsedText : "", 
-        updateParsedText : (text) => set({ parsedText : text })
+        updateParsedText : (text) => set({ parsedText : text }),
+        jobs : [],
+        updateJobs :(jobs) => set({ jobs }),
      }
 ))
 
