acd9e1f609173564faef300b1e5e37c31631e539
daman599 <damanpreetk117@gmail.com>
2025-08-05 18:53:47 +0530

    authentication added

---
+++ lib/authOptions.ts
@@ -0,0 +1,19 @@
+import { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const options:AuthOptions = {
+ providers: [
+  GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      },
+  })
+],
+secret: process.env.NEXTAUTH_SECRET,
+}
