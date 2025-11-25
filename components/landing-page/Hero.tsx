0034740266f7c8a55465f2540f99326d47963c20
daman599 <damanpreetk117@gmail.com>
2025-11-25 23:29:46 +0530

    subtle animation added

--- components/landing-page/Hero.tsx
+++ components/landing-page/Hero.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import CTAButton from "./CTA-Button";
 import { inter } from "@/lib/fonts";
+import { motion } from "motion/react";
 
 export default function Hero() {
   return (
@@ -8,23 +11,29 @@ export default function Hero() {
                  bg-cover bg-center flex items-center justify-center">
       <div className="absolute inset-0 z-0" />
 
-      <div className="relative z-10 flex flex-col md:flex-row items-center justify-between 
+      <motion.div
+        initial={{ opacity: 0, filter: "blur(3px)" }}
+        whileInView={{ opacity: 1, filter: "blur(0px)" }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+        viewport={{ once: true }}
+
+        className="relative z-10 flex flex-col md:flex-row items-center justify-between 
                       w-full max-w-7xl mx-auto gap-10 sm:gap-12">
-        
+
         <div className="max-w-xl text-center md:text-left space-y-6">
-          
-          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight text-white"> 
+
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight text-white">
             Your Resume,<br />Perfect Jobs.<br />Powered by AI.
           </h1>
-          
-          <p className={`text-base sm:text-lg md:text-xl text-gray-300 font-sans ${inter.variable}`}>
+
+          <p className={`text-base sm:text-lg text-gray-300 font-sans ${inter.variable}`}>
             Revolutionizing your job search. Our intelligent AI matches your resume with the ideal
             opportunities, saving you time and boosting your career.
           </p>
-          
+
           <CTAButton />
-        </div>    
-      </div>
+        </div>
+      </motion.div>
     </section>
   );
 }
