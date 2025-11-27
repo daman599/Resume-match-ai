cec04dd0e9330b0345c84a630f01192c7cd262ac
daman599 <damanpreetk117@gmail.com>
2025-11-28 00:33:12 +0530

    few fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import  Hero from "@/components/landing-page/Hero"
-import  HowItWorks from "@/components/landing-page/HowItWorks"
-import  Features from "@/components/landing-page/Features"
+import Hero from "@/components/landing-page/Hero"
+import HowItWorks from "@/components/landing-page/HowItWorks"
+import Features from "@/components/landing-page/Features"
 
 export default function Home() {
   return (
-   <main className="bg-black text-white min-h-screen relative">
-    <div
-      className="w-full bg-cover bg-center pt-24 bg-no-repeat"
-      style={{ backgroundImage: "url('/bg-network.jpg')" }}
-    >
-      <Hero/>
-    </div>
-    <HowItWorks/>
-    <Features/>
+    <main className="bg-black text-white min-h-screen relative">
+      <div
+        className="w-full bg-cover bg-center pt-24 bg-no-repeat"
+        style={{ backgroundImage: "url('/bg-network.jpg')" }}
+      >
+        <Hero />
+      </div>
+      <HowItWorks />
+      <Features />
     </main>
   );
 }
--- components/helperComponents/Loader.tsx
+++ components/helperComponents/Loader.tsx
@@ -1,7 +1,7 @@
-import { ReactNode } from "react";
+import React from "react";
 
 interface PropType {
-  children: ReactNode;
+  children: React.ReactNode;
 }
 
 export default function Loader({ children }: PropType) {
