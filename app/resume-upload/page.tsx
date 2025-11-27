15ffd98974e5a0ba5467b90fc6ed06399411dc54
daman599 <damanpreetk117@gmail.com>
2025-11-27 23:15:53 +0530

    fade in animation added for smooth user experience

--- app/resume-upload/page.tsx
+++ app/resume-upload/page.tsx
@@ -8,6 +8,7 @@ import { useRouter } from 'next/navigation';
 import { useStore } from "@/lib/state-store";
 import Loader from "@/components/helperComponents/Loader";
 import ErrorComponent from "@/components/helperComponents/Error";
+import { motion } from "motion/react";
 
 export default function ResumeUpload() {
 
@@ -70,56 +71,54 @@ export default function ResumeUpload() {
   }
 
   return (
-    <>
-      <div className="relative min-h-screen bg-black overflow-hidden pt-16">
-        <div className="relative z-10 flex flex-col sm:flex-row items-center justify-center min-h-[calc(100vh-64px)] px-4 sm:px-6 md:px-8">
+    <motion.div
+      initial={{ opacity: 0, filter: "blur(3px)" }}
+      whileInView={{ opacity: 1, filter: "blur(0px)" }}
+      transition={{ duration: 0.8, ease: "easeInOut" }}
+      className="relative min-h-screen bg-black overflow-hidden pt-16">
+      <div className="relative z-10 flex flex-col sm:flex-row items-center justify-center min-h-[calc(100vh-64px)] px-4 sm:px-6 md:px-8">
 
-          <div
-            {...getRootProps()}
-            className="w-full 
-        max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl 
+        <div
+          {...getRootProps()}
+          className="w-full 
+        max-w-sm sm:max-w-md md:max-w-lg 
         p-4 sm:p-6 md:p-8                    
         rounded-3xl border border-white/20 border-dashed 
         bg-white/5 backdrop-blur-xl shadow-2xl 
         flex flex-col items-center justify-center 
         space-y-4 text-center
         hover:border-blue-400 transition-all duration-300"
-          >
-            <input {...getInputProps()} />
-            <UploadCloud className="text-[#0096FF] animate-pulse" size={40} />
-            <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-white">
-              Drag and drop your resume here
-            </h1>
-            <p className="text-xs sm:text-sm md:text-base text-gray-300 max-w-sm">Or</p>
-            <button
-              onClick={() => inputRef.current?.click()}
-              className="px-4 py-2 sm:px-6 sm:py-3  
+        >
+          <input {...getInputProps()} />
+          <UploadCloud className="text-[#0096FF] animate-pulse" size={40} />
+          <h1 className="text-lg sm:text-xl md:text-2xl font-semibold text-white">
+            Drag and drop your resume here
+          </h1>
+          <span className="text-xs sm:text-sm md:text-base text-gray-300 max-w-sm">Or</span>
+          <button
+            onClick={() => inputRef.current?.click()}
+            className="px-4 py-2 sm:px-6 sm:py-3  
           text-xs sm:text-sm md:text-base    
           rounded-md 
           bg-white/10 border border-white/20 text-white font-medium
           hover:bg-white/20 hover:shadow-md focus:outline-none
           focus:ring-2 focus:ring-white/30 transition duration-200 cursor-pointer"
-            >
-              Select File
-            </button>
-            <input
-              type="file"
-              ref={inputRef}
-              accept=".pdf"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                const file = e.target.files?.[0];
-                if (file) helper(file);
-              }}
-              className="hidden"
-            />
-          </div>
-
-          <div className="block sm:hidden mt-4 text-center text-sm text-gray-400 ">
-            <p >Works well on large screens.</p>
-          </div>
+          >
+            Select File
+          </button>
+          <input
+            type="file"
+            ref={inputRef}
+            accept=".pdf"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              const file = e.target.files?.[0];
+              if (file) helper(file);
+            }}
+            className="hidden"
+          />
         </div>
+      </div>
 
-      </div >
-    </>
+    </motion.div>
   );
 }
