3f2ce1623799dfeda962d9d4db555cc02cd431a9
daman599 <damanpreetk117@gmail.com>
2025-11-30 14:01:44 +0530

    few improvements in client component

--- components/landing-page/Hero.tsx
+++ components/landing-page/Hero.tsx
@@ -1,23 +1,20 @@
 "use client"
 
-import CTAButton from "./CTA-Button";
 import { inter } from "@/lib/fonts";
 import { motion } from "motion/react";
+import { UploadCloud } from 'lucide-react';
+import Link from 'next/link';
 
 export const Hero = () => {
   return (
-    <section className="relative min-h-[calc(100vh-64px)] px-4 sm:px-6 md:px-12 py-16 sm:py-20 
-                 bg-cover bg-center flex items-center justify-center">
-
+    <section className="relative min-h-[calc(100vh-64px)] px-4 sm:px-6 md:px-12 py-16 sm:py-20 bg-cover bg-center flex items-center justify-center">
       <motion.div
         initial={{ opacity: 0, filter: "blur(3px)" }}
         whileInView={{ opacity: 1, filter: "blur(0px)" }}
         transition={{ duration: 0.8, ease: "easeInOut" }}
         viewport={{ once: true }}
-
-        className="relative z-10 flex flex-col md:flex-row items-center justify-between 
-                      w-full max-w-7xl mx-auto gap-10 sm:gap-12">
-
+        className="relative z-10 flex flex-col md:flex-row items-center justify-between w-full max-w-7xl mx-auto gap-10 sm:gap-12"
+      >
         <div className="max-w-xl text-center md:text-left space-y-6">
 
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight text-white">
@@ -29,7 +26,14 @@ export const Hero = () => {
             opportunities, saving you time and boosting your career.
           </p>
 
-          <CTAButton />
+          <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/resume-upload`}
+            className="inline-flex items-center gap-2 px-4 sm:px-6 py-2.5 sm:py-3 
+                   border border-white text-white font-semibold rounded-md 
+                   hover:bg-white hover:text-black transition-all duration-200 cursor-pointer" >
+            <UploadCloud className="w-4 h-4 sm:w-5 sm:h-5" />
+            <span className="text-sm sm:text-base">Upload Your Resume</span>
+          </Link>
+
         </div>
       </motion.div>
     </section>
